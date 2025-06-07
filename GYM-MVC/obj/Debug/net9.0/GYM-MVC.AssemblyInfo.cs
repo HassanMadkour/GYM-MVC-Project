58ed9c647a20eae9a5bc0a772ae05fe34e8570bb
Fatma Adel Youssef <fatmaeed2001@gmail.com>
2025-06-07 21:05:12 +0300

    ya

--- GYM-MVC/obj/Debug/net9.0/GYM-MVC.AssemblyInfo.cs
+++ GYM-MVC/obj/Debug/net9.0/GYM-MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GYM-MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7b416fed14d3623f4a0f841003366839b2e9ae0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa31715ecb7ee7b40e3dffa43b13f79599dd6db2")]
 [assembly: System.Reflection.AssemblyProductAttribute("GYM-MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GYM-MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
