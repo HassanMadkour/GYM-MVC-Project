e0d85656fc9630123e0afe87c6f7dafdb6052ec9
HassanMadkour <hassan20elmsriy@gmail.com>
2025-06-08 13:06:44 +0300

    ensure that everything is updated

--- GYM-MVC/obj/Debug/net9.0/GYM-MVC.AssemblyInfo.cs
+++ GYM-MVC/obj/Debug/net9.0/GYM-MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GYM-MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+430705619b1c273fb3c8bf147cab94b637ecd6f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2769fcd86929f1d5b71b87de083c02ea466f32a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GYM-MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GYM-MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
