a2a98fa2cc78f8dcf240b1186f6784d02d9b67fc
Rahma Fatthy <rahmafatthy2001@gmail.com>
2025-06-14 01:14:19 +0300

    enhance registerition

---
+++ GYM-MVC/ViewModels/MembershipViewModels/MembershipViewModel.cs
@@ -0,0 +1,14 @@
+﻿using GYM_MVC.Core.Entities;
+
+namespace GYM_MVC.ViewModels.MembershipViewModels
+{
+    public class MembershipViewModel
+    {
+        public int Id { get; set; }
+        public MembershipType Type { get; set; }
+        public decimal Price { get; set; }
+        public int DurationInDays { get; set; }
+
+        public string Description { get; set; }
+    }
+}
