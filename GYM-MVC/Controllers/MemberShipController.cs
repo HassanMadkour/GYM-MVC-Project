cde6e3d10d4bc8c04d75341a566c48bc670d1bac
HassanMadkour <hassan20elmsriy@gmail.com>
2025-06-14 12:30:18 +0300

    implement create action

--- GYM-MVC/Controllers/MemberShipController.cs
+++ GYM-MVC/Controllers/MemberShipController.cs
@@ -1,11 +1,20 @@
-﻿using GYM_MVC.Core.Entities;
+﻿using AutoMapper;
+using GYM_MVC.Core.Entities;
 using GYM_MVC.Core.Helper;
+using GYM_MVC.Core.IUnitOfWorks;
 using GYM_MVC.ViewModels.MembershipViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GYM_MVC.Controllers {
 
     public class MemberShipController : Controller {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
+
+        public MemberShipController(IUnitOfWork unitOfWork, IMapper mapper) {
+            this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
+        }
 
         [HttpGet]
         public IActionResult Create() {
@@ -15,10 +24,11 @@ namespace GYM_MVC.Controllers {
         }
 
         [HttpPost]
-        public IActionResult Create(CreateMembershipViewModel model) {
+        public async Task<IActionResult> Create(CreateMembershipViewModel model) {
             model.MembershipTypeList = EnumHelper.ToSelectList<MembershipType>();
             if (!ModelState.IsValid) return View(model);
-
+            await unitOfWork.MembershipRepo.Add(mapper.Map<Membership>(model));
+            await unitOfWork.Save();
             return RedirectToAction("Index", "Home");
         }
     }
