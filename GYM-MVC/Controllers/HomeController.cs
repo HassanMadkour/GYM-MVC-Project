2d69c429b52e5c3e63fae1ec1c0081bc367adba0
HassanMadkour <hassan20elmsriy@gmail.com>
2025-06-19 13:17:18 +0300

    commit

--- GYM-MVC/Controllers/HomeController.cs
+++ GYM-MVC/Controllers/HomeController.cs
@@ -1,19 +1,56 @@
+using AutoMapper;
+using GYM_MVC.Core.IUnitOfWorks;
+using GYM_MVC.Data.UnitOfWorks;
 using GYM_MVC.Models;
+using GYM_MVC.ViewModels.ScheduleViewModels;
+using GYM_MVC.ViewModels.TrainerViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace GYM_MVC.Controllers {
 
     public class HomeController : Controller {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger) {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly IMapper mapper;
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork,IMapper mapper )
+        {
             _logger = logger;
+            this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
         }
 
-        public IActionResult Index() {
+      
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var schedules = await Task.Run(() => unitOfWork.ScheduleRepo.GetAll().ToList());
+            var scheduleViewModels = mapper.Map<List<ScheduleViewModel>>(schedules);
+            var trainrs = mapper.Map < List < DisplayTrainerVM >> (await Task.Run(() => unitOfWork.TrainerRepo.GetAll().ToList()));
+
+            var scheduleTable = scheduleViewModels
+                .GroupBy(s => s.Time.ToString(@"hh\:mm"))
+                .Select(g => new
+                {
+                    Time = g.Key,
+                    Monday = string.Join(", ", g.Where(s => s.DayOfWeek.Equals("Monday", StringComparison.OrdinalIgnoreCase)).Select(s => s.ClassName)),
+                    Tuesday = string.Join(", ", g.Where(s => s.DayOfWeek.Equals("Tuesday", StringComparison.OrdinalIgnoreCase)).Select(s => s.ClassName)),
+                    Wednesday = string.Join(", ", g.Where(s => s.DayOfWeek.Equals("Wednesday", StringComparison.OrdinalIgnoreCase)).Select(s => s.ClassName)),
+                    Thursday = string.Join(", ", g.Where(s => s.DayOfWeek.Equals("Thursday", StringComparison.OrdinalIgnoreCase)).Select(s => s.ClassName)),
+                    Friday = string.Join(", ", g.Where(s => s.DayOfWeek.Equals("Friday", StringComparison.OrdinalIgnoreCase)).Select(s => s.ClassName)),
+                    Saturday = string.Join(", ", g.Where(s => s.DayOfWeek.Equals("Saturday", StringComparison.OrdinalIgnoreCase)).Select(s => s.ClassName)),
+                    Sunday = string.Join(", ", g.Where(s => s.DayOfWeek.Equals("Sunday", StringComparison.OrdinalIgnoreCase)).Select(s => s.ClassName))
+                })
+                .OrderBy(s => s.Time)
+                .ToList();
+            ViewBag.Trainers = trainrs;
+            ViewBag.ScheduleTable = scheduleTable;
             return View();
         }
+
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
--- GYM-MVC/ViewModels/TrainerViewModels/MemberByTrainerIdVM.cs
+++ GYM-MVC/ViewModels/TrainerViewModels/MemberByTrainerIdVM.cs
@@ -4,6 +4,8 @@
         public string Name { get; set; }
         public int Id { get; set; }
         public int Age { get; set; }
+        public string MaritalStatus { get; set; }
         public string Injuries { get; set; }
+
     }
 }
